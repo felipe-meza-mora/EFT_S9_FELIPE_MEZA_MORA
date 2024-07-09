57533f033c6bb9ecf11689867316c32e8efa9b78
felipe-meza-mora <feli.meza@duocuc.cl>
2024-07-09 12:55:45 -0400

    Commit Pruebas unitarias
    Se configurar pruebas unitarias para trabajar con firestore
    -Orders
    -Sales

--- dyf/src/app/pages/orders/orders.component.spec.ts
+++ dyf/src/app/pages/orders/orders.component.spec.ts
@@ -1,14 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { OrderService } from '../../service/order.service';
+import { appConfig } from '../../app.config';
+import { of } from 'rxjs'
 import { OrdersComponent } from './orders.component';
+import { or } from '@angular/fire/firestore';
 
 describe('OrdersComponent', () => {
   let component: OrdersComponent;
   let fixture: ComponentFixture<OrdersComponent>;
+  let orderService: OrderService;
 
   beforeEach(async () => {
+    const orderServiceMock = {
+      getOrders: jasmine.createSpy('getOrders').and.returnValue(of([])),
+      updateOrders: jasmine.createSpy('updateOrders').and.returnValue(Promise.resolve())
+    };
+
     await TestBed.configureTestingModule({
-      imports: [OrdersComponent]
+      imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: orderService, useValue: orderServiceMock },
+        ...appConfig.providers, // Usa todos los proveedores de appConfig
+      ]
     })
     .compileComponents();
     
@@ -20,4 +40,5 @@ describe('OrdersComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
 });
--- dyf/src/app/pages/sales/sales.component.spec.ts
+++ dyf/src/app/pages/sales/sales.component.spec.ts
@@ -1,14 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { OrderService } from '../../service/order.service';
+import { appConfig } from '../../app.config';
+import { of } from 'rxjs'
 import { SalesComponent } from './sales.component';
+import { or } from '@angular/fire/firestore';
+
 
 describe('SalesComponent', () => {
   let component: SalesComponent;
   let fixture: ComponentFixture<SalesComponent>;
+  let orderService: OrderService;
 
   beforeEach(async () => {
+    const orderServiceMock = {
+      getOrders: jasmine.createSpy('getOrders').and.returnValue(of([])),
+      updateOrders: jasmine.createSpy('updateOrders').and.returnValue(Promise.resolve())
+    };
+
     await TestBed.configureTestingModule({
-      imports: [SalesComponent]
+      imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: orderService, useValue: orderServiceMock },
+        ...appConfig.providers, // Usa todos los proveedores de appConfig
+      ]
     })
     .compileComponents();
     
